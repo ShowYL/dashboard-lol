c1efd8bb34151d336a2a286b0a2031759a2fcd1a
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-01-04 20:53:57 +0100

    Add title and meta description to app.html; enhance input handling in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,115 +1,138 @@
 <script>
-    import { onMount } from 'svelte';
-    import { handwriting } from '$lib/components/handwritting.js';
+	import { onMount } from 'svelte';
+	import { handwriting } from '$lib/components/handwritting.js';
 
-    let { data } = $props();
+	let { data } = $props();
 
-    let bannerUrl = data.fetchedPantryData.banner;
-    let textDisplayed = $state(false);
-    let researchClicked = $state(false);
-    let input = $state('');
-    let showText = $state(false); // for transition purpose by entering the dom after the initialitation
+	let bannerUrl = data.fetchedPantryData.banner;
+	let textDisplayed = $state(false);
+	let researchClicked = $state(false);
+	let input = $state(null);
+	let inputValue = $state('');
+	let inputDiv = $state(null);
+	let showText = $state(false); // for transition purpose by entering the dom after the initialitation
 
-    $effect(() => {
-        if (researchClicked) {
-            input?.focus();
-        }
-    });
+	$effect(() => {
+		if (researchClicked) {
+			input?.focus();
+		}
+	});
 
-    onMount(() => {
-        window.addEventListener('keydown', handleKeydown);
-        window.addEventListener('click', handleClick);
-        showText = true; // Trigger the transition on mount
-    });
+	onMount(() => {
+		window.addEventListener('keydown', handleKeydown);
+		window.addEventListener('click', handleClick);
+		showText = true; // Trigger the transition on mount
+	});
 
-    function handleKeydown(event) {
-        if (event.ctrlKey && event.key === 'k') {
-            event.preventDefault();
-            researchClicked = !researchClicked;
-        }
-    }
+	function handleKeydown(event) {
+		if (event.ctrlKey && event.key === 'k') {
+			event.preventDefault();
+			researchClicked = !researchClicked;
+		}
+	}
 
-    function handleClick(event) {
-        if (researchClicked && event.target != input) {
-            console.log('here');
-            researchClicked = false;
-        }
-    }
+	function handleClick(event) {
+		if (researchClicked && inputDiv && !inputDiv.contains(event.target)) {
+			researchClicked = false;
+		}
+	}
 
-    let backgroundImage = $derived(
-        `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 20%, rgba(0, 0, 0, 1) 100%), url(${bannerUrl})`
-    );
+	let backgroundImage = $derived(
+		`linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3) 40%,rgba(0,0,0,0.7) 65%,rgba(0,0,0,0.9) 75%, rgba(0, 0, 0, 1) 100%), url(${bannerUrl})`
+	);
 </script>
 
 <div
-    class="flex h-full w-full items-center justify-center bg-cover bg-center bg-no-repeat"
-    style="background-image: {backgroundImage};"
+	class="flex h-full w-full items-center justify-center bg-cover bg-center bg-no-repeat"
+	style="background-image: {backgroundImage};"
 >
-    <div class="mx-auto mb-20 mt-auto flex h-1/5 w-2/5 items-center justify-center text-slate-400">
-        {#if !researchClicked}
-            <button
-                class="flex w-3/5 min-w-56 items-center rounded bg-slate-900 p-1 text-left opacity-70 shadow-2xl"
-                onclick={(event) => {
-                    event.stopPropagation();
-                    researchClicked = true;
-                }}
-            >
-                {#if showText}
-                    <span in:handwriting|global={{ speed: 1.2 }} onintroend={() => (textDisplayed = true)}>
-                        Hide on bush#GOAT
-                    </span>
-                {/if}
-                {#if textDisplayed}
-                    <span class="ml-auto">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                            <g
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                            >
-                                <path
-                                    stroke-dasharray="64"
-                                    stroke-dashoffset="64"
-                                    d="M3 12c0 4.97 4.03 9 9 9c4.97 0 9 -4.03 9 -9c0 -4.97 -4.03 -9 -9 -9c-4.97 0 -9 4.03 -9 9Z"
-                                >
-                                    <animate
-                                        fill="freeze"
-                                        attributeName="stroke-dashoffset"
-                                        dur="0.33s"
-                                        values="64;0"
-                                    />
-                                </path>
-                                <path stroke-dasharray="12" stroke-dashoffset="12" d="M7 12h9.5">
-                                    <animate
-                                        fill="freeze"
-                                        attributeName="stroke-dashoffset"
-                                        begin="0.385s"
-                                        dur="0.11s"
-                                        values="12;0"
-                                    />
-                                </path>
-                                <path stroke-dasharray="8" stroke-dashoffset="8" d="M17 12l-4 4M17 12l-4 -4">
-                                    <animate
-                                        fill="freeze"
-                                        attributeName="stroke-dashoffset"
-                                        begin="0.495s"
-                                        dur="0.11s"
-                                        values="8;0"
-                                    />
-                                </path>
-                            </g>
-                        </svg>
-                    </span>
-                {/if}
-            </button>
-        {:else}
-            <input
-                bind:this={input}
-                class="flex w-3/5 min-w-56 rounded bg-slate-900 p-1 text-left opacity-70 shadow-2xl focus:border-none"
-                placeholder="riotID + #TAG"
-            />
-        {/if}
-    </div>
+	<div class="mx-auto mb-20 mt-auto flex h-1/5 w-2/5 items-center justify-center text-slate-400">
+		{#if !researchClicked}
+			<button
+				class="flex w-3/5 min-w-56 items-center rounded bg-slate-900 p-1 text-left opacity-70 shadow-2xl"
+				onclick={(event) => {
+					event.stopPropagation();
+					researchClicked = true;
+				}}
+			>
+				{#if showText}
+					<span in:handwriting|global={{ speed: 1.2 }} onintroend={() => (textDisplayed = true)}>
+						Hide on bush#GOAT
+					</span>
+				{/if}
+				{#if textDisplayed}
+					<span class="ml-auto">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+							<g
+								fill="none"
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+							>
+								<path
+									stroke-dasharray="64"
+									stroke-dashoffset="64"
+									d="M3 12c0 4.97 4.03 9 9 9c4.97 0 9 -4.03 9 -9c0 -4.97 -4.03 -9 -9 -9c-4.97 0 -9 4.03 -9 9Z"
+								>
+									<animate
+										fill="freeze"
+										attributeName="stroke-dashoffset"
+										dur="0.33s"
+										values="64;0"
+									/>
+								</path>
+								<path stroke-dasharray="12" stroke-dashoffset="12" d="M7 12h9.5">
+									<animate
+										fill="freeze"
+										attributeName="stroke-dashoffset"
+										begin="0.385s"
+										dur="0.11s"
+										values="12;0"
+									/>
+								</path>
+								<path stroke-dasharray="8" stroke-dashoffset="8" d="M17 12l-4 4M17 12l-4 -4">
+									<animate
+										fill="freeze"
+										attributeName="stroke-dashoffset"
+										begin="0.495s"
+										dur="0.11s"
+										values="8;0"
+									/>
+								</path>
+							</g>
+						</svg>
+					</span>
+				{/if}
+			</button>
+		{:else}
+			<div
+				bind:this={inputDiv}
+				class="flex w-3/5 min-w-56 rounded bg-slate-900 p-1 text-left opacity-70 shadow-2xl"
+			>
+				<input
+					bind:this={input}
+					bind:value={inputValue}
+					class="w-5/6 rounded bg-slate-900 p-1 focus:border-none focus:outline-none"
+					placeholder="riotID + #TAG"
+				/>
+				<button
+					class="flex w-1/6 items-center justify-end rounded"
+					aria-label="button to search player"
+					><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path
+								d="M3 12c0 4.97 4.03 9 9 9c4.97 0 9 -4.03 9 -9c0 -4.97 -4.03 -9 -9 -9c-4.97 0 -9 4.03 -9 9Z"
+							/><path d="M7 12h9.5" /><path d="M17 12l-4 4M17 12l-4 -4" /></g
+						></svg
+					></button
+				>
+			</div>
+		{/if}
+	</div>
 </div>
